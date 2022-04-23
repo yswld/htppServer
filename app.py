e04aa07e06370f73908326153fb9ca77e117bc99
yoshida.yusuke <yyusukerad@gmail.com>
2022-04-23 09:44:52 +0900

    ポート設定を追加

--- app.py
+++ app.py
@@ -66,3 +66,10 @@ def get_json_from_dictionary():
         'key2': 'val2'
     }
     return jsonify(dic)  # JSONをレスポンス
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
